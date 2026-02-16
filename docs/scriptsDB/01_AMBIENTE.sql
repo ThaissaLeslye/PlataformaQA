f218967c31363162ec415a4f8faa30706bea95fa
thaissa <thaissa.leslye.tl@gmail.com>
2026-02-16 11:42:58 -0300

    adiciona server node com ts

---
+++ docs/scriptsDB/01_AMBIENTE.sql
@@ -0,0 +1,7 @@
+-- -----------------------------------------------------
+-- Configuração do Schema e Sessão
+-- -----------------------------------------------------
+
+DROP SCHEMA IF EXISTS qualidade_db;
+CREATE SCHEMA IF NOT EXISTS qualidade_db DEFAULT CHARACTER SET utf8;
+USE qualidade_db;
